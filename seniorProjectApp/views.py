f45d43eaa56d77c9558d59edd22c8683fb369446
mpreyes <[reyes.madelyn.mr@gmail.com]>
2019-02-09 22:23:44 -0600

    added template to project

--- seniorProjectApp/views.py
+++ seniorProjectApp/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello Senior Project!")
+    return render(request, 'seniorProjectApp/index.html')
--- seniorproject/settings.py
+++ seniorproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'seniorProjectApp.apps.SeniorprojectappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'seniorProjectApp/static/'
