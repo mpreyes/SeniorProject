78a3b1c4445a7c68b14d2705be098cd39b3b5373
mpreyes <[reyes.madelyn.mr@gmail.com]>
2019-02-15 15:12:02 -0600

    broke up template into logical pieces, created relevant pages

--- seniorProjectApp/urls.py
+++ seniorProjectApp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('links/', views.links, name='links'),
 ]
--- seniorProjectApp/views.py
+++ seniorProjectApp/views.py
@@ -6,3 +6,14 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'seniorProjectApp/index.html')
+
+
+def signup(request):
+    return render(request,'seniorProjectApp/signup.html')
+
+def dashboard(request):
+    return render(request,'seniorProjectApp/dashboard.html')
+
+def links(request):
+    return render(request,'seniorProjectApp/links.html')
+
