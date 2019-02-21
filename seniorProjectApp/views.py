873662c008ef3fd74f8cb41e0f30a386aa1d6a76
mpreyes <[reyes.madelyn.mr@gmail.com]>
2019-02-21 11:04:50 -0600

    started work on dashboard

--- seniorProjectApp/views.py
+++ seniorProjectApp/views.py
@@ -12,7 +12,12 @@ def signup(request):
     return render(request,'seniorProjectApp/signup.html')
 
 def dashboard(request):
-    return render(request,'seniorProjectApp/dashboard.html')
+    #add any field you want passed to the dashboard.html page
+    classes_list = [{"name": "intro to programming", "color": "blue", "link" :"in_prog"},{"name": "class2", "color": "blue"},{"name": "class3", "color": "blue"},{"name": "class4", "color": "blue"}, {"name": "class5", "color": "blue"},{"name": "class6", "color": "blue"}]
+    context ={"classes_list": classes_list}
+    #print("context " + str(context["classes_list"]))
+
+    return render(request,'seniorProjectApp/dashboard.html',context)
 
 def links(request):
     return render(request,'seniorProjectApp/links.html')
