c2e0f8dd07fd4541a7a55e66d44af0b14c15d465
mpreyes <[reyes.madelyn.mr@gmail.com]>
2019-02-24 19:24:13 -0600

    adding links to links pg, beginning cache config

--- seniorProjectApp/urls.py
+++ seniorProjectApp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('links/', views.links, name='links'),
+    
 ]
--- seniorProjectApp/views.py
+++ seniorProjectApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.cache import caches
 
 # Create your views here.
 
@@ -12,13 +13,26 @@ def signup(request):
     return render(request,'seniorProjectApp/signup.html')
 
 def dashboard(request):
-    #add any field you want passed to the dashboard.html page
-    classes_list = [{"name": "intro to programming", "color": "blue", "link" :"in_prog"},{"name": "class2", "color": "blue"},{"name": "class3", "color": "blue"},{"name": "class4", "color": "blue"}, {"name": "class5", "color": "blue"},{"name": "class6", "color": "blue"}]
-    context ={"classes_list": classes_list}
+    #ERIN: add any field you want passed to the dashboard.html page
+    level_freshman = [{"name": "intro to programming", "description": "intro  to programming teaches you the basics of programming, including..", "color":
+     "blue", "links" : "1"},{"name": "class2", "description": "intro  to programming teaches you the basics of programming, including..", "color": "blue"},
+     {"name": "class3", "description": "intro  to programming teaches you the basics of programming, including..","color": "blue", },{"name": "class4", 
+      "description": "intro  to programming teaches you the basics of programming, including..", "color": "blue"}, {"name": "class5", "description": "intro  to programming teaches you the basics of programming, including..",
+       "color": "blue"},{"name": "class6",  "description": "intro  to programming teaches you the basics of programming, including..",
+       "color": "blue"}]
+
+    #MR: set cache to pass in whatever level we want to show
+    #MR TODO: Create a class to represent our entire class/link relationship
+    #caches['levels'].set('level_freshman', level_freshman) 
+    context ={"classes_list": level_freshman}
     #print("context " + str(context["classes_list"]))
 
     return render(request,'seniorProjectApp/dashboard.html',context)
 
 def links(request):
-    return render(request,'seniorProjectApp/links.html')
+   
+    #context = {"levels":  caches['levels'].get('level_freshman')}
+    class_1_links = [ "link1", "link","link2","link2","link3","link3","link4","linky"]
+    context = {"links": class_1_links}
+    return render(request,'seniorProjectApp/links.html',context)
 
